8f41c82437827c81bd9618bc0cee6dedd5db88dd
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-09-27 15:09:40 +0800

    AboutController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'AboutController@index')->name('about');
