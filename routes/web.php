ec145c418db493c2e1bd68d2b692754cf59533ec
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-09-27 15:13:06 +0800

    ContactController

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/', 'AboutController@index')->name('about');
+Route::get('/', 'ContactController@index')->name('contact');
