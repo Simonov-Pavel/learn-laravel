e450200c0b4275b68af9f100187d6044db29da63
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-09-27 17:27:34 +0800

    Создание таблицы сообщений

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 			'unix_socket' => env('DB_SOCKET', ''),
 			'charset' => 'utf8',
 			'collation' => 'utf8_unicode_ci',
-			'prefix' => 'simon',
+			'prefix' => 'simon_',
 			'prefix_indexes' => true,
 			'strict' => true,
 			'engine' => null,
--- database/migrations/2021_09_27_091549_create_contacts_table.php
+++ database/migrations/2021_09_27_091549_create_contacts_table.php
@@ -6,26 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateContactsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('contacts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('contacts', function (Blueprint $table) {
+			$table->bigIncrements('id');
+			$table->string('name');
+			$table->string('email');
+			$table->string('subject');
+			$table->text('message');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('contacts');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('contacts');
+	}
 }
