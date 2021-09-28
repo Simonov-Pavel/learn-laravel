e843be7a9bfe784b7ae761a7281c37123839944d
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-09-28 22:42:47 +0800

    Вывод всех сообщений из бд

---
+++ resources/views/messages.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title', 'Все сообщения')
+
+@section('content')
+<h1>Все сообщения</h1>
+@foreach($data as $item)
+<div class="alert alert-info">
+	<h3>{{$item->subject}}</h3>
+	<p>{{$item->name}} - {{$item->created_at}}</p>
+	<p><small>{{$item->email}}</small></p>
+	<a href="{{route('message-show')}}" class='btn btn-success'>Подробнее</a>
+</div>
+@endforeach
+@endsection
