1d5ea15edccb3a976a4945f436bcd4102591c5fb
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-09-28 22:47:09 +0800

    Передача в route id сообщения

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -9,7 +9,7 @@
 	<h3>{{$item->subject}}</h3>
 	<p>{{$item->name}} - {{$item->created_at}}</p>
 	<p><small>{{$item->email}}</small></p>
-	<a href="{{route('message-show')}}" class='btn btn-success'>Подробнее</a>
+	<a href="{{route('message-show', $item->id)}}" class='btn btn-success'>Подробнее</a>
 </div>
 @endforeach
 @endsection
